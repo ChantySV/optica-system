49463a2b297050055e23f3014c856da417a30fb8
ChantySV <Santiago_SV@outlook.es>
2024-12-04 20:02:34 -0400

    Config Inicial

---
+++ src/config/env.config.ts
@@ -0,0 +1,7 @@
+export const EnvConfiguration = () =>({
+        port: process.env.PORT || 4000,
+        dbName: process.env.DB_NAME,
+        dbPassword: process.env.DB_PASSWORD,
+        dbHost: process.env.DB_HOST,
+        dbUserName: process.env.DB_USERNAME,
+})
